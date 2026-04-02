57a5184a2763c9e5f2180d9a9ae523ee9024975a
Fynn Koch <mail@fynn-koch.de>
2026-04-02 16:08:21 +0200

    Add about us section

--- src/app/pages/about-us/about-us.ts
+++ src/app/pages/about-us/about-us.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-about-us',
-  imports: [MatCardModule],
   templateUrl: './about-us.html',
   styleUrl: './about-us.scss',
 })
