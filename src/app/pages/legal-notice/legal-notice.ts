87707cf4c424b1011364f7f19d4d3fbc1b0f5310
Fynn Koch <mail@fynn-koch.de>
2026-04-04 09:36:25 +0200

    Add legal-notice & privacy policy pages

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [MatCardModule],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [MatCardModule],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
